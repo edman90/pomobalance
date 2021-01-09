a15c06f1b6c26f976c37ea425b931cde93a46691
Edward Pedroza <epedroza@purdue.edu>
2021-01-09 16:35:21 -0600

    Began code of countdown function defining variables and expressions using let statements

--- countdown.js
+++ countdown.js
@@ -1,1 +1,13 @@
-function 
+function startCountdown(minutesChosen) {
+
+    let timeSec = minutesChosen * 60;
+
+    let setCdInterval = setInterval(function() {
+        let displaySeconds = timeSec % 60;
+        let displayMinutes = Math.floor(timeSec / 60);
+
+        countdownBox.innerHTML = `${displayMinutes} : ${displaySeconds}`;
+    }
+    )
+
+}
