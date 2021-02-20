bd528ccee1d9f5bb2ceaee3b973467093e483186
Edward Pedroza <epedroza@purdue.edu>
2021-02-20 11:56:27 -0600

    code for integrating dropdown menu for total time

--- index.js
+++ index.js
@@ -60,3 +60,21 @@ function renderBackToStart() {
     hideElements(countdownBox, cancelBox, videoBox, breakTime);
     unhideElements(infoDesc, selBox, timeBox);
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
